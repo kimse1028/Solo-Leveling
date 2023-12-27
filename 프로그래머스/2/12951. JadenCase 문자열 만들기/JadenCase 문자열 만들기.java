53a3b886edf92d581acf8cd524d0b286c7c703b5
KimSe1028 <113516054+kimse1028@users.noreply.github.com>
2023-12-27 17:51:17 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 2.34 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,33 +1,27 @@
-import java.util.*;
-
 class Solution {
     public String solution(String s) {
         String answer = "";
-    	
-    	// " " 기준으로 문자열 잘라서 배열에 넣음
-    	String[] arr = s.split(" ");
-    	// 잘린 문자열을 순서대로 처리
-    	for(int i=0; i<arr.length; i++) {
-    		String now = arr[i];
-    		
-    		// 문자열의 길이가 0이라면(공백 이라면)
-    		// answer에 " "만 추가
-    		if(arr[i].length() == 0) {
+        
+        String[] sArray = s.split(" ");
+        
+        for(int i = 0; i < sArray.length; i++) {
+            String x = sArray[i];
+            
+            //split으로 공백이 있을 수 있으므로 조건 추가
+            if(!x.isEmpty()) {
+                //첫 글자 대문자로 변경
+                answer += x.substring(0, 1).toUpperCase();
+                //첫 글자 제외 나머지 소문자로 변경
+                answer += x.substring(1, x.length()).toLowerCase();
+                
+                //다음 문자열 사이 공백 추가
+                answer += " ";
+                
+            } else {
     			answer += " ";
     		} 
-    		// 문자가 있다면
-    		else {
-    			// 0번째 문자는 대문자로
-    			answer += now.substring(0, 1).toUpperCase();
-    			// 1번째 문자부터 마지막까지는 소문자로
-    			answer += now.substring(1, now.length()).toLowerCase();
-    			// 마지막에 " " 추가
-    			answer += " ";
-    		}
-    		
-    	}
-    	
-    	// 입력 받은 문자열의 맨 마지막이 " " 라면 바로 answer 반환
+        }
+        // 입력 받은 문자열의 맨 마지막이 " " 라면 바로 answer 반환
     	if(s.substring(s.length()-1, s.length()).equals(" ")){
     		return answer;
     	}
@@ -35,5 +29,4 @@ class Solution {
     	// 맨 마지막 " " 제거하고 answer 반환
         return answer.substring(0, answer.length()-1);
     }
-
 }
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951?language=java) 
 
 ### 성능 요약
 
-메모리: 73 MB, 시간: 2.78 ms
+메모리: 74.2 MB, 시간: 2.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 3일 17:46:10
+2023년 12월 3일 17:51:15
 
 ### 문제 설명
 
